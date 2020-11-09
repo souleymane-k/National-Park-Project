f563e0e2808905fb74827c032ef6e74519d99bad
souleymane kone <desolo74@gmail.com>
2020-11-08 21:15:13 -0800

    finalChanged

--- app.js
+++ app.js
@@ -14,26 +14,29 @@ function displayResults(responseJson){
     $('#results-list').empty();
     for(let i=0; i<responseJson.data.length; i++){
         $('#results-list').append(
-    `<li><h2><${responseJson.data[i].fullName}</h2>
-    <p>${responseJson.data[i].description}</p>
-    <p>${responseJson.data[i].url}</p>  
+    `<li>
+    <hr>
+         <p>${responseJson.data[i].fullName}</p>
+         <p>${responseJson.data[i].description}</p>
+         <a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a> 
+         
     </li>
-        `)
-    };
+        `
+    )};
 
-    $('#results').removeClass('.hidden');
+    $('#results').removeClass('hidden');
 }
 function getParkList(query, maxResults =10){ 
     const parasms = {
         api_key: apiKey,
-              q:query,
-              maxResults
+        q:query,
+        maxResults
+         
         
     };
     const queryString = formatQueryParasms(parasms)
     const url = searchURL+'?'+queryString;
-
-
+  console.log(url);
 fetch(url)
     .then(response =>{
         if(response.ok){
